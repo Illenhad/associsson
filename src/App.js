a38457b31d0fd60a2407c8e6937f238afdc0cc1e
Pierre Legrand <illenhad@MacBook-Pro-de-Pierre.local>
2021-04-28 21:11:52 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Projet en cours de construction.
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/Illenhad/associsson"
+          target="_blank"
+          rel="author"
+        >
+          Suivez-nous
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
