7c293a4d92ab6c6bbbf046e8be95c592f2eea0d8
illenhad <pierrelegrand.pro@gmail.com>
2021-05-01 20:45:21 +0200

    Initialize a responsible Navbar.

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,37 @@
+import React, {useState, useEffect} from 'react';
+import './Navbar.css';
+
+function Navbar() {
+
+    const [toggleMenu, setToggleMenu] = useState(false);
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    const toggleMenuSmallScreen = () => {
+        setToggleMenu(!toggleMenu);
+    }
+
+    useEffect(() => {
+        const changeWidth = () => {
+            setWindowWidth(window.innerWidth);
+            if (window.innerWidth > 500) { setToggleMenu(false); }
+        };
+
+        window.addEventListener('resize', changeWidth);
+        return () => { window.removeEventListener('resize', changeWidth); }
+    }, [toggleMenu]);
+
+    return (
+        <nav>
+            {(toggleMenu || windowWidth > 500) && (
+                <ul className="navbar">
+                    <li className="navbar-items">Page 1</li>
+                    <li className="navbar-items">Page 2</li>
+                    <li className="navbar-items">Page 3</li>
+                </ul>
+            )}
+            <button onClick={toggleMenuSmallScreen} className="navbar-btn">BTN</button>
+        </nav>
+    );
+}
+
+export default Navbar;
