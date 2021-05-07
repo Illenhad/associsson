e4d64960e5d219776c8f2ec132d952ca5aa0718d
Charline <charline.henneron@viacesi.fr>
2021-05-07 19:18:31 +0200

    Ajout projet - form sans CSS

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,29 @@ import './App.css';
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import ProjectList from "./container/ProjectList/ProjectList"
+import AddProject from "./components/addProject/AddProject";
+
 function App() {
-    return (
-        <div className="App">
-            <Navbar/>
-            {/*<header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Site en cours de construction.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://github.com/Illenhad/associsson"
-                    rel="author"
-                >
-                    Suivez-nous
-                </a>
-            </header>*/}
-                <ProjectList/>
-            
-            <Footer/>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Navbar/>
+      <AddProject/>
+      {/*<header className="App-header">
+          <img src={logo} className="App-logo" alt="logo"/>
+          <p>
+              Site en cours de construction.
+          </p>
+          <a
+              className="App-link"
+              href="https://github.com/Illenhad/associsson"
+              rel="author"
+          >
+              Suivez-nous
+          </a>
+      </header>*/}
+      <Footer/>
+    </div>
+  );
 }
 
 export default App;
