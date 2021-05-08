53945aa9fab35a63daa3822f06fffe85345de0f9
illenhad <pierrelegrand.pro@gmail.com>
2021-05-08 15:20:13 +0200

    [U] Adopt the color scheme into the navbar and the footer.

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,16 @@
-import logo from './logo.png';
 import './App.css';
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import ProjectList from "./container/ProjectList/ProjectList"
+
 function App() {
     return (
         <div className="App">
             <Navbar/>
-            {/*<header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Site en cours de construction.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://github.com/Illenhad/associsson"
-                    rel="author"
-                >
-                    Suivez-nous
-                </a>
-            </header>*/}
+            <header className="App-header">
                 <ProjectList/>
-            
+            </header>
+
             <Footer/>
         </div>
     );
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faDiscord, faGithub} from '@fortawesome/free-brands-svg-icons'
 import "./Footer.css";
 
@@ -9,27 +9,29 @@ function Footer() {
 
     return (
         <footer>
-            <div className="footer-info">
-                <img className="footer-logo" alt="Logo" src={process.env.PUBLIC_URL + "images/favicon.ico"}/>
-                <p className="app-version">Version {pjson.version}</p>
-            </div>
-            <ul className="footer-section">
-                <li>Accueil</li>
-                <li>Liste des projets</li>
-                <li>Se connecter</li>
-            </ul>
-            <ul className="footer-section">
-                <li>Mentions Légales</li>
-                <li>Protections des données</li>
-                <li>Nous contacter</li>
-            </ul>
-            <div className="footer-section footer-social">
-                <a href="https://discord.gg/5Fgy6kSe">
-                    <FontAwesomeIcon icon={faDiscord} size={"2x"}/>
-                </a>
-                <a href="https://github.com/Illenhad/associsson">
-                    <FontAwesomeIcon icon={faGithub} size={"2x"}/>
-                </a>
+            <div className="footer-container">
+                <div className="footer-info">
+                    <img className="footer-logo" alt="Logo" src={process.env.PUBLIC_URL + "images/meta/favicon.ico"}/>
+                    <p className="app-version">Version {pjson.version}</p>
+                </div>
+                <ul className="footer-section">
+                    <li>Accueil</li>
+                    <li>Liste des projets</li>
+                    <li>Se connecter</li>
+                </ul>
+                <ul className="footer-section">
+                    <li>Mentions Légales</li>
+                    <li>Protections des données</li>
+                    <li>Nous contacter</li>
+                </ul>
+                <div className="footer-section footer-social">
+                    <a href="https://discord.gg/5Fgy6kSe">
+                        <FontAwesomeIcon icon={faDiscord} size={"2x"}/>
+                    </a>
+                    <a href="https://github.com/Illenhad/associsson">
+                        <FontAwesomeIcon icon={faGithub} size={"2x"}/>
+                    </a>
+                </div>
             </div>
         </footer>
     );
